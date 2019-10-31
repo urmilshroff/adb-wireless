b5ec5f9129e40b9ff0b6aefdae91353d76bfe263
urmilshroff <urmilshroff@gmail.com>
2019-10-31 22:57:49 +0530

    Splashed some color

--- mac/adb-wireless.sh
+++ mac/adb-wireless.sh
@@ -1,19 +1,26 @@
 #!/bin/zsh
 
-FIRST_DEVICE=$(adb devices | awk 'NR==2')
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+DEFAULT="\033[0m"
+
+FIRST_DEVICE=$(adb devices | awk "NR==2")
 
 if [ -z "$FIRST_DEVICE" ]; then
-    echo -e "Error: no device found!"
+    echo -e "\n${RED}Error: no device found!${DEFAULT}"
     echo -e "1. Ensure your phone and computer are on the same Wi-Fi network"
-    echo -e "2. Connect your phone to this computer via a USB cable"
-    echo -e "3. Run the script again!"
+    echo -e "2. Make sure USB debugging is enabled"
+    echo -e "3. Connect your phone via a USB cable"
+    echo -e "4. Run the script again"
+    echo -e "5. You can then disconnect the cable\n"
     
 else
-    echo -e "Starting ADB Wireless..."
+    echo -e "\n${YELLOW}Starting ADB Wireless by Urmil Shroff...${DEFAULT}"
     IP_ADDRESS=$(adb shell "ip addr show wlan0 | grep -e wlan0$ | cut -d\" \" -f 6 | cut -d/ -f 1")
     adb tcpip 5555
-    sleep 2
+    sleep 1
     adb connect $IP_ADDRESS:5555
-    echo -e "To disconnect your phone, run 'adb disconnect'."
-    echo -e "You can remove the cable if you haven't already!"
+    echo -e "To disconnect your phone, run ${YELLOW}adb disconnect${DEFAULT}."
+    echo -e "${GREEN}You can remove your cable if you haven't already!${DEFAULT}\n"
 fi
