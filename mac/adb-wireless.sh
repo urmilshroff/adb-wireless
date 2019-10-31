4cec51d96a97692f4ce5c1da269bd3cca2f46301
urmilshroff <urmilshroff@gmail.com>
2019-10-31 21:33:21 +0530

    Update adb-wireless.sh

--- mac/adb-wireless.sh
+++ mac/adb-wireless.sh
@@ -9,11 +9,11 @@ if [ -z "$FIRST_DEVICE" ]; then
     echo -e "3. Run the script again!"
     
 else
-    echo -e "Initializing adb-wireless..."
+    echo -e "Starting ADB Wireless..."
     IP_ADDRESS=$(adb shell "ip addr show wlan0 | grep -e wlan0$ | cut -d\" \" -f 6 | cut -d/ -f 1")
     adb tcpip 5555
     sleep 2
     adb connect $IP_ADDRESS:5555
-    echo -e "You can remove the cable if you haven't already!"
     echo -e "To disconnect your phone, run 'adb disconnect'."
+    echo -e "You can remove the cable if you haven't already!"
 fi
