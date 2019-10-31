c256e7724bb212f1e6c4e052b9d2867c689e88dd
urmilshroff <urmilshroff@gmail.com>
2019-10-31 21:03:06 +0530

    Basic working script

--- mac/adb-wireless.sh
+++ mac/adb-wireless.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 
-echo -e "Hello! I'm adb-wireless."
+echo -e "Initializing adb-wireless..."
+IP_ADDRESS=$(adb shell "ip addr show wlan0 | grep -e wlan0$ | cut -d\" \" -f 6 | cut -d/ -f 1")
+adb tcpip 5555
+sleep 2
+adb connect $IP_ADDRESS:5555
+echo -e "Great! You can now remove the cable."
+echo -e "To disconnect your phone, run 'adb disconnect'."
