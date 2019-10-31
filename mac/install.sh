d7aa8392428041c3c598ee1cde48e635c5e9849c
urmilshroff <urmilshroff@gmail.com>
2019-10-31 22:34:45 +0530

    Add executable to PATH

--- mac/install.sh
+++ mac/install.sh
@@ -6,7 +6,13 @@ if [ -f ~/Library/Android/sdk/platform-tools/adb-wireless ]; then
 fi
 
 echo -e "Installing adb-wireless..."
+
 cp adb-wireless.sh ~/Library/Android/sdk/platform-tools
 mv ~/Library/Android/sdk/platform-tools/adb-wireless.sh ~/Library/Android/sdk/platform-tools/adb-wireless
 chmod +x ~/Library/Android/sdk/platform-tools/adb-wireless
+
+PATH='PATH="/Users/$USER/Library/Android/sdk/platform-tools/:${PATH}"'
+echo "$PATH" >> ~/.zprofile
+echo "export PATH" >> ~/.zprofile
+
 echo -e "Done! Run the script anytime using 'adb-wireless'."
