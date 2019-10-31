15720546e4c7ebd9906cae2a698d7159b9cadff9
urmilshroff <urmilshroff@gmail.com>
2019-10-31 20:15:55 +0530

    Created basic installer

---
+++ mac/adb-wireless.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+echo -e "Hello! I'm adb-wireless."
---
+++ mac/install.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+if [ -f ~/Library/Android/sdk/platform-tools/adb-wireless ]; then
+    echo -e "Removing old version..."
+    rm ~/Library/Android/sdk/platform-tools/adb-wireless
+fi
+
+echo -e "Installing adb-wireless..."
+cp adb-wireless.sh ~/Library/Android/sdk/platform-tools
+mv ~/Library/Android/sdk/platform-tools/adb-wireless.sh ~/Library/Android/sdk/platform-tools/adb-wireless
+chmod +x ~/Library/Android/sdk/platform-tools/adb-wireless
+echo -e "Done! Run the script anytime using 'adb-wireless'."
